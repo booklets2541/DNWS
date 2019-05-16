6e4dbfe5f2963564543269dfb2b637c2b70c0a91
NOPPANUT CHANTARAMANEE <booklet2541@gmail.com>
2019-05-16 16:45:38 +0700

    add follow and unfollow

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -7,8 +7,20 @@ angular.module('followingList', ['ngRoute'])
       var self = this;
 
       const requestOptions = {
-          headers: { 'X-session': $rootScope.x_session }
+          headers: { 'X-session': $rootScope.x_session } //600611030 600611034 give advice
+
       };
+
+        self.sendfollow = function sendfollow(username) {
+
+            const data = "followingname=" + encodeURIComponent(username);
+            $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions);
+        }
+        self.sendunfollow = function sendunfollow(username) {
+            $http.defaults.headers.delete = { 'X-session': $rootScope.x_session };
+            const data = "followingname=" + encodeURIComponent(username);
+            $http.delete('http://localhost:8080/twitterapi/following/?' + data);
+        }
 
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
